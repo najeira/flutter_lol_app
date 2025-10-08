49ced471cf80f2fd540c41a9c43977df7f342fc7
najeira <najeira@gmail.com>
2025-10-08 14:50:18 +0900

    refactor

--- lib/utils/json.dart
+++ lib/utils/json.dart
@@ -1,11 +1,9 @@
-int asInt(dynamic value, {int defaultValue = 0}) {
+int asInt(dynamic value, [int defaultValue = 0]) {
   if (value is int) {
     return value;
-  }
-  if (value is num) {
+  } else if (value is num) {
     return value.toInt();
-  }
-  if (value is String) {
+  } else if (value is String) {
     final parsed = int.tryParse(value);
     if (parsed != null) {
       return parsed;
@@ -14,14 +12,12 @@ int asInt(dynamic value, {int defaultValue = 0}) {
   return defaultValue;
 }
 
-double asDouble(dynamic value, {double defaultValue = 0}) {
+double asDouble(dynamic value, [double defaultValue = 0]) {
   if (value is double) {
     return value;
-  }
-  if (value is num) {
+  } else if (value is num) {
     return value.toDouble();
-  }
-  if (value is String) {
+  } else if (value is String) {
     final parsed = double.tryParse(value);
     if (parsed != null) {
       return parsed;
@@ -30,27 +26,24 @@ double asDouble(dynamic value, {double defaultValue = 0}) {
   return defaultValue;
 }
 
-bool asBool(dynamic value, {bool defaultValue = false}) {
+bool asBool(dynamic value, [bool defaultValue = false]) {
   if (value is bool) {
     return value;
-  }
-  if (value is String) {
+  } else if (value is String) {
     final lower = value.toLowerCase();
     if (lower == 'true') {
       return true;
-    }
-    if (lower == 'false') {
+    } else if (lower == 'false') {
       return false;
     }
   }
   return defaultValue;
 }
 
-String asString(dynamic value, {String defaultValue = ''}) {
+String asString(dynamic value, [String defaultValue = '']) {
   if (value is String) {
     return value;
-  }
-  if (value != null) {
+  } else if (value != null) {
     return value.toString();
   }
   return defaultValue;
